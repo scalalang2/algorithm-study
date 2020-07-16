fb8dfe107c807c6663b82870a81758aeed4ef378
DoHyeon Lee <cailhuiris@gmail.com>
2020-07-16 10:59:50 +0900

    Baekjoon - 2439

---
+++ trial/problem2439.go
@@ -0,0 +1,23 @@
+package main
+
+import "fmt"
+
+func main() {
+	var N int
+
+	fmt.Scanf("%d", &N)
+
+	for i := 0; i < N; i++ {
+		text := ""
+
+		for j := 0; j < N-i-1; j++ {
+			text = text + " "
+		}
+
+		for k := 0; k < i+1; k++ {
+			text = text + "*"
+		}
+
+		fmt.Println(text)
+	}
+}
