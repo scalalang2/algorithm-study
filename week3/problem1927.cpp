e3932db5efc25282fba4e840442818994c661afd
DoHyeon Lee <cailhuiris@gmail.com>
2020-07-30 16:38:24 +0900

    Baekjoon - Problem 1927

---
+++ week3/problem1927.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+#include <vector>
+#include <queue>
+
+using namespace std;
+
+int main() {
+    int N, op;
+    priority_queue<int, vector<int>,  greater<int> > queue;
+
+    scanf("%d", &N);
+
+    while(N--) {
+        scanf("%d", &op);
+        if(op == 0) {
+            if(queue.empty()) {
+                printf("0\n");
+            } else {
+                printf("%d\n", queue.top());
+                queue.pop();
+            }
+        } else {
+            queue.push(op);
+        }
+    }
+}
