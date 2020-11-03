83d64d642d0e01e818d7ea64c448ca109836645d
DoHyeon Lee <cailhuiris@gmail.com>
2020-11-03 09:23:57 +0900

    Added p9251

---
+++ gold/p9251.cpp
@@ -0,0 +1,21 @@
+// 출처: http://melonicedlatte.com/algorithm/2018/03/15/181550.html
+
+#include<cstdio>
+#include<algorithm>
+
+using namespace std;
+
+char s1[1002], s2[1002];
+int dp[1001][1001], i, j;
+
+int main() {
+    scanf("%s %s", s1 + 1, s2 + 1);
+    for (i = 1; s1[i]; i++)
+        for (j = 1; s2[j]; j++)
+            dp[i][j] = max({ dp[i][j - 1], dp[i - 1][j],
+                             dp[i - 1][j - 1] + (s1[i] == s2[j]) 
+                           });
+    printf("%d", dp[i - 1][j - 1]);
+
+    return 0;
+}
